3287c6b76e039bfaa97aaa34593c8aaa25cc9ba5
brdy1 <brdy724@gmail.com>
2020-03-06 22:46:22 -0700

    logo grayscale

---
+++ podcast.php
@@ -0,0 +1,49 @@
+
+  <?php require_once('includes/codehead.php');?>
+
+<h1>Brady's Portfolio</h1>
+<h2>Podcast</h2>
+
+<?php include('includes/menu.php');?>
+
+</header>
+
+<main class="main">
+
+<h3>I have chosen to cancel and remove Hermetic Garden from this website. Expect the button to disappear soon.</h3>
+
+
+<p></p>
+
+
+
+</main>
+
+<aside class="sidebar">
+
+<h3>Quick Links</h3>
+
+<h4>Thesis</h4>
+<ul>
+	<li><a href="thesis.php">Introduction</a></li>
+	<li><a href="alchemica.php">"Rosa Alchemica"</a></li>
+	<li><a href="tables.php">"The Tables of the Law"</a></li>
+	<li><a href="magi.php">"The Adoration of the Magi"</a></li>
+	<li><a href="bib.php">Bibliography</a></li>
+</ul>
+
+<?php include('includes/podcast.php');?>
+
+</aside>
+
+<br class="clear">
+
+<footer class="footer">
+ 
+	<?php include('includes/codefoot.php');?>
+ 
+</footer>
+
+</div>
+</body>
+</html>
