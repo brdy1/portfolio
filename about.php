7017393deb208a9a9340a4d425f4c16cebb4b07a
brdy1 <brdy724@gmail.com>
2023-03-14 10:57:05 -0700

    no idea

--- about.php
+++ about.php
@@ -12,11 +12,7 @@
 
 <h2>Background</h2>
 
-<p>I grew up in Grants Pass, Oregon and graduated in the class of 2005 from Grants Pass High School. After that, I attended Eastern Washington University in Cheney, Washington where I spent seven years obtaining my bachelor's and master's degrees. I then moved to sunny (and by contrast to my native area, dry) Phoenix, Arizona. I spent two years as a teacher, but left the field to pursue a career in libraries. Now I work as a Library Information Specialist Lead at Arizona State University in Tempe, Arizona.</p>
-
-<h2>Interests</h2>
-
-<p>In my free time I read books, play some video games, run campaigns in Pathfinder RPG, and I love spending time with my family during holidays. Through my passion for reading I've acquired a formidable library in my spare bedroom.</p>
+<p></p>
 
 </main>
 
--- includes/codefoot.php
+++ includes/codefoot.php
@@ -7,7 +7,7 @@
 <footer>
 <?php
 echo "<p>&copy;" . date("Y") . " Brady Peneton</p>";
-?>
+?><span id="siteseal"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=4n0d0QaYEHhZJDovYTwF9DhVDHXnVKrKDU8upyHf5LHf1lP4jHQ1Jw5S04Lu"></script></span>
 </footer>
 </div>
 
